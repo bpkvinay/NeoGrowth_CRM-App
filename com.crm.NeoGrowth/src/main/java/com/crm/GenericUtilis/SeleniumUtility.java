3dc81f7ea5e791e65fc0310e04175b524497e765
Pavan.Joshi <bpo-pavan.joshi@neogrowth.in>
2023-05-12 17:40:03 +0530

    generic selenium utility commit

--- com.crm.NeoGrowth/src/main/java/com/crm/GenericUtilis/SeleniumUtility.java
+++ com.crm.NeoGrowth/src/main/java/com/crm/GenericUtilis/SeleniumUtility.java
@@ -191,11 +191,38 @@ public class SeleniumUtility extends Driver {
 											else 
 											{
 												forwardbutton1.click();
-											}
+												{
 										}
+											}}
 										//day.click();
+											}
+										
+										/**
+										 * @author Pavan.Joshi
+										 * This method Used to handle the windows and child windows
+										 * 
+										 */
+										public void windowHandle() {
+										String parent=driver.getWindowHandle();
+										Set<String>s=driver.getWindowHandles();
+										  Iterator<String> I1= s.iterator();{
+										 while(I1.hasNext())
+										{
+										String child_window=I1.next();
+										if(!parent.equals(child_window))
+										{
+										driver.switchTo().window(child_window);
+										System.out.println(driver.switchTo().window(child_window).getTitle());
+										}
+										}
+										  }
+
+
 									}
- 				}
+									}
+ 				
+
+
 
        
 
