6aff1d7ad30d4c159214ed4ac672938c0043106f
BPO Sitrarasan S <BPO-Sitrarasan.S@neogrowth.in>
2023-05-15 11:29:42 +0530

    Cases Filterpage

--- com.crm.NeoGrowth/src/test/java/com/crm/NeoGrowthstepDef/CasesStepDef.java
+++ com.crm.NeoGrowth/src/test/java/com/crm/NeoGrowthstepDef/CasesStepDef.java
@@ -1,5 +1,7 @@
 package com.crm.NeoGrowthstepDef;
 
+import static org.testng.Assert.assertEquals;
+
 import java.io.IOException;
 
 import javax.xml.xpath.XPath;
@@ -8,6 +10,7 @@ import org.apache.poi.EncryptedDocumentException;
 
 import com.crm.GenericUtilis.Driver;
 import com.crm.GenericUtilis.SeleniumUtility;
+import com.crm.POM.CasesFilterPage;
 import com.crm.POM.CasesPage;
 
 import io.cucumber.java.en.And;
@@ -15,197 +18,158 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import junit.framework.Assert;
 
-
 public class CasesStepDef extends Driver{
 	
-	
-Driver dr = new Driver();
-	
+	Driver dr = new Driver();
 	SeleniumUtility su = new SeleniumUtility();
 	CasesPage cp = new CasesPage(driver);
+	CasesFilterPage cf = new CasesFilterPage(driver);
 	
-	  @When("^click on the cases module link$")
+	    @When("^click on the cases module link$")
 	    public void click_on_the_cases_module_link() {
-		  cp.ClickCasesLink().click();
-	       
+		  cp.ClickCasesLink().click();    
 	    }
 	 
-	  @Then("^View Cases Page Should be Display$")
+	    @Then("^View Cases Page Should be Display$")
 	    public void view_cases_page_should_be_display() throws EncryptedDocumentException, IOException {
 		  String Actual = dr.getexceldata("Titles", 4, 1);
 		  String Expected = driver.getTitle();
-		  Assert.assertEquals(Expected, Actual);
-		  	  
-	        
+		  Assert.assertEquals(Expected, Actual);   
 	    }
-
-	   @When("^User Click on Pencil Icon$")
-	    public void user_click_on_pencil_icon()  {
+	  
+	    @When("^User Click on Pencil Icon$")
+	    public void user_click_on_pencil_icon() throws Throwable  {
 		   cp.ClickPencilIcon().click();
-		        
+		   Thread.sleep(2000);
 	    }
 
-	   @When("Entering the value in TATDaysTF {string}")
-	   public void entering_the_value_in_tat_days_tf(String Tatdaysvalue) {
-		   cp.getTatDaysTF(Tatdaysvalue);
-		   
-	      
-	   }
+	    @And("^Entering the value in TATDaysTF (.+)$")
+	    public void entering_the_value_in_tatdaystf(String Tatdaysvalue) throws Throwable {
+	    	cp.getTatDaysTF(Tatdaysvalue);   
+	    }
 	    	
-	    @And("^Click on Case Category dropdown and select(.+) option$")
+	    @And("^Click on Case Category dropdown and select (.+) option$")
 	    public void click_on_case_category_dropdown_and_select_option(String Casecategory){
-	    	cp.ClickCaseCategoryDropdown(Casecategory);
-	        
+	    	cp.ClickCaseCategoryDropdown(Casecategory);   
 	    }
-
-	    @And("^Click on Case Sub Category dropdown and select(.+) option$")
+	    
+	    @And("^Click on Case Sub Category dropdown and select (.+) option$")
 	    public void click_on_case_sub_category_dropdown_and_select_option(String subcategory) {
 	        cp.ClickCaseSubCategoryDropdown(subcategory);
 	    }
 
-	    @And("^Click on Case Details dropdown and select(.+) option$")
+	    @And("^Click on Case Details dropdown and select (.+) option$")
 	    public void click_on_case_details_dropdown_and_select_option(String Casedetails) {
-	    	cp.ClickCaseDetailsDropdown(Casedetails);
-	        
+	    	cp.ClickCaseDetailsDropdown(Casedetails);    
 	    }
 	    
 	    @Then("^User Scrolldown click SaveBtn$")
 	    public void user_scrolldown_click_savebtn()  {
 	    	cp.ClickSaveButton();
 	    }
-	    
-	  ////////////////////////////////////////////////////////
-	    ///////////////////////////////////////////
-	    
+	  	    
 	    @When("^Click on CasesModule Link$")
-	    public void click_on_casesmodule_link() {
-	    	cp.clickCasesModuleLink().click();
-	        
-	    }
-	   
-	    @When("^Click on the Filter Icon$")
-	    public void click_on_the_filter_icon() throws InterruptedException  {
-	    	cp.clickFilterIcon().click();
-	    	Thread.sleep(2000);
-	        
+	    public void click_on_casesmodule_link() throws Throwable {
+	    	cf.ClickCasesModuleLink().click();  
 	    }
 
-	    @Then("^Advance Filter Tab Should be Display$")
-	    public void advance_filter_tab_should_be_display() throws EncryptedDocumentException, IOException {
-	    	String Actual = dr.getexceldata("Titles", 5, 1);
-	    	String Expected = driver.getTitle();
-	    	Assert.assertEquals(Expected, Actual);
-	    	 
+	    @When("^Click on the Filter Icon$")
+	    public void click_on_the_filter_icon() throws Throwable {
+	    	cf.ClickFilterIcon().click();   
+	    }
+
+	    @Then("^Advance FilterTab Should be Display$")
+	    public void advance_filtertab_should_be_display() throws Throwable {  
 	    }
 	    
 	    @And("^Click on the AdvanceFilter button$")
-	    public void click_on_the_advancefilter_button(){
-	        cp.clickAdvanceFilterBtn().click();
+	    public void click_on_the_advancefilter_button() throws Throwable {
+	       cf.ClickAdvanceFilterBtn().click();
 	    }
-	   
+
 	    @And("^Enter the Value in NumberTF (.+)$")
-	    public void enter_the_value_in_numbertf(String Number) throws InterruptedException  {
-	    	cp.enterNumberTF(Number);
-	    	Thread.sleep(2000);
-	        
+	    public void enter_the_value_in_numbertf(String Number) throws Throwable {
+	    	cf.EnterNumberTF(Number);   
 	    }
 
 	    @And("^Enter the value in SubjectTF (.+)$")
-	    public void enter_the_value_in_subjecttf(String Subject) {
-	    	cp.enterSubjectTF(Subject);
-	        
-	    }
-	    
-	    @And("^Select the value from Statusbox (.+)$")
-	    public void select_the_value_from_statusbox(String status) {
-	        cp.selectStatusBox(status);
-	    }
-	    
-	    @And("^Select the value from AssignedtoBox (.+)$")
-	    public void select_the_value_from_assignedtobox(String assignedto) throws InterruptedException {
-	    	cp.selectAssignedtoBox(assignedto);
-	    	Thread.sleep(2000);
-	       
+	    public void enter_the_value_in_subjecttf(String Subject) throws Throwable {
+	    	cf.EnterSubjectTF(Subject);    
 	    }
 
-	    @And("^User Scrolldown and Enter the value in CaseownerTF (.+)$")
-	    public void user_scrolldown_and_enter_the_value_in_caseownertf(String Caseowner) throws InterruptedException  {
-	    	cp.enterCaseownerTF(Caseowner);
-	    	Thread.sleep(2000);
-	        
+	    @And("^Select the value from Statusbox (.+)$")
+	    public void select_the_value_from_statusbox(String status) throws Throwable {
+	    	cf.SelectStatusBox(status);   
+	    }
+
+	    @And("^Select the value from AssignedtoBox (.+)$")
+	    public void select_the_value_from_assignedtobox(String Assignedto) throws Throwable {
+	       cf.SelectAssignedtoBox(Assignedto);
+	    }
+	   
+	    @And("^Enter the value in CaseownerTF (.+)$")
+	    public void enter_the_value_in_caseownertf(String caseowner) throws Throwable {
+	        cf.EnterCaseownerTF(caseowner);
 	    }
 
 	    @And("^Select the value in PriorityBox (.+)$")
-	    public void select_the_value_in_prioritybox(String priority) throws InterruptedException {
-	    	cp.selectPriorityBox(priority);
-	    	Thread.sleep(3000);
-	        
+	    public void select_the_value_in_prioritybox(String priority) throws Throwable {
+	    	cf.SelectPriorityBox(priority);    
 	    }
 
 	    @And("^Enter the value in AppidTF (.+)$")
-	    public void enter_the_value_in_appidtf(String Appid) {
-	    	cp.enterAppidTF(Appid);
-	        
+	    public void enter_the_value_in_appidtf(String appid) throws Throwable {
+	    	cf.EnterAppidTF(appid);    
 	    }
 
 	    @And("^Enter the  value in NameTF (.+)$")
-	    public void enter_the_value_in_nametf(String Name) {
-	    	cp.enterNameTF(Name);
-	       
+	    public void enter_the_value_in_nametf(String name) throws Throwable {
+	    	cf.EnterNameTF(name);   
 	    }
 
 	    @And("^Enter the value in EmailidTF (.+)$")
-	    public void enter_the_value_in_emailidtf(String Emailid) {
-	    	cp.enterEmailidTF(Emailid);
-	        
+	    public void enter_the_value_in_emailidtf(String emailid) throws Throwable {
+	    	cf.EnterEmailidTF(emailid);   
 	    }
 
 	    @And("^Enter the value in ContactTF (.+)$")
-	    public void enter_the_value_in_contacttf(String Contact) {
-	    	cp.enterContactnumberTF(Contact);
-	       
+	    public void enter_the_value_in_contacttf(String contact) throws Throwable {
+	        cf.EnterContactnumberTF(contact);
 	    }
 
 	    @And("^Enter the value in LocationTF (.+)$")
-	    public void enter_the_value_in_locationtf(String location) {
-	    	cp.selectLocationBox(location);
-	        
+	    public void enter_the_value_in_locationtf(String location) throws Throwable {
+	        cf.SelectLocationBox(location);
 	    }
 
-	    @And("^User Scrolldown and Select the value in TypeBox (.+)$")
-	    public void user_scrolldown_and_select_the_value_in_typebox(String Type){
-	    	cp.selectTypeBox(Type);
-	        
+	    @And("^Select the value in TypeBox (.+)$")
+	    public void select_the_value_in_typebox(String type) throws Throwable {
+	    	cf.SelectTypeBox(type);   
 	    }
 
-	    @And("^User Scrolldown Enter the value in AgeTF (.+)$")
-	    public void user_scrolldown_enter_the_value_in_agetf(String Age) throws Throwable {
-	    	Thread.sleep(2000);
-	    	cp.enterAgeTF(Age);
-	    	Thread.sleep(3000);
+	    @And("^Enter the value in AgeTF (.+)$")
+	    public void enter_the_value_in_agetf(String age) throws Throwable {
+	    	cf.EnterAgeTF(age);
 	        
 	    }
 
 	    @And("^Select the value in CasecategoryBox (.+)$")
-	    public void select_the_value_in_casecategorybox(String Casecategory) throws InterruptedException {
-	    	cp.selectCasecategoryBox(Casecategory);
-	    	Thread.sleep(5000);
-	       
+	    public void select_the_value_in_casecategorybox(String casecategory) throws Throwable {
+	        cf.SelectCasecategoryBox(casecategory);
 	    }
 	    
-	    @When("User ScrollDown and Click on Save button")
-	    public void user_scroll_down_and_click_on_save_button() throws Throwable {
-	    	Thread.sleep(3000);
-	    	cp.clickSavebutton();
-	    	Thread.sleep(5000);
-	    	
-	    }
-	    
-	    @Then("^Search Result should be display$")
-	    public void search_result_should_be_display() {
-	       
+	    @And("^Select the value in EmailsourceBox (.+)$")
+	    public void select_the_value_in_emailsourcebox(String emailsource) throws Throwable {
+	        cf.SelectEmailsourceBox(emailsource);
 	    }
 
-	    
-	   
+	    @When("^User ScrollDown and Click on Save button$")
+	    public void user_scrolldown_and_click_on_save_button() throws Throwable {
+	    	cf.ClickSavebutton();
+	    }
+
+	    @Then("^Search Result should be display (.+)$")
+	    public void search_result_should_be_display(String searchresult) throws Throwable {
+	    	Assert.assertEquals(cf.getSearchResult(), searchresult);
+	    }
 }
